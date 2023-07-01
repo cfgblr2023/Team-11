1ce3c1b1279a4aa200984fe0a4ae02f700c302de
Aadigoel <aadigoel114@gmail.com>
2023-07-02 02:55:15 +0530

    api with basic frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import Home from "./pages/Home/Home";
 import './App.css';
 import Learning from './pages/Learning/Learning';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ViewCourses from './studentComponents/viewCourses';
+import Login from './studentComponents/login';
 
+import Display from "./pages/displayData/display";
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path = "/" element= {<Home/>}/>
         <Route path="/learning" element={<Learning />} />
+        <Route path="/courses" element={<ViewCourses />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/display" element={<Display/>} />
       </Routes>
     </Router>
   );
